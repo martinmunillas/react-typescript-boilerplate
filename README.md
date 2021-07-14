ab645eae5c6e3b1b40f3f1eba5b254408414d637
martinmunillas <martinmunillas@gmail.com>
2021-07-14 19:09:14 -0400

    feature: created boilerplate

---
+++ README.md
@@ -0,0 +1,8 @@
+# React Typescript Boilerplate
+
+## Quick start
+
+1. Clone the repo
+2. Install the dependencies using `yarn`
+3. Start the developing `yarn dev`
+4. Go to [http://localhost:8080](http://localhost:8080)
---
+++ src/app.tsx
@@ -0,0 +1,13 @@
+import React from 'react';
+
+import ReactLogo from '@images/react.svg';
+
+const App: React.FC = () => {
+    return (
+        <div>
+            <ReactLogo width="500px" height="500px" />
+            <h1>React Boilerplate</h1>
+        </div>
+    );
+};
+export default App;
